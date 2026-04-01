27ab392a40da42d27b4e231846619f49ee6cd44f
0713jake <0713jake@naver.com>
2026-04-01 16:29:43 +0900

    Extracted amountFor method

--- src/main/java/com/videorental/Customer.java
+++ src/main/java/com/videorental/Customer.java
@@ -27,27 +27,10 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 
 		while ( iterator.hasNext() ) {
-			double thisAmount = 0;
 			Rental each = (Rental) iterator.next();
 			// determine amounts for each line
 
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-				
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-				
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
+			double thisAmount = amountFor(each);
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -64,5 +47,27 @@ class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter pointers";
 
 		return result;
+	}
+
+	private static double amountFor(Rental each) {
+		double thisAmount = 0;
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	}
 }
