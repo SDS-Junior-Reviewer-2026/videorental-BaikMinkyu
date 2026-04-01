5c2b8c95d1dc1c24f975276c8341e6cd2f6414e1
0713jake <0713jake@naver.com>
2026-04-01 16:16:42 +0900

    Extracted createRentalFor method and changed arrangement to be inline

--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -29,10 +29,7 @@ public class CustomerTest {
     @Test
     public void testRentalDaysRented2() {
         // arrange
-        Movie movie = new Movie(TITLE, Movie.REGULAR);
-        int daysRented = 2;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(2, Movie.REGULAR));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t2.0(TITLE_NOT_IMPORTANT)\nAmount owed is 2.0\nYou earned 1 frequent renter pointers", customer.statement());
@@ -41,10 +38,7 @@ public class CustomerTest {
     @Test
     public void testRentalDaysRented3() {
         // arrange
-        Movie movie = new Movie(TITLE, Movie.REGULAR);
-        int daysRented = 3;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(3, Movie.REGULAR));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t3.5(TITLE_NOT_IMPORTANT)\nAmount owed is 3.5\nYou earned 1 frequent renter pointers", customer.statement());
@@ -53,10 +47,7 @@ public class CustomerTest {
     @Test
     public void testMoviePriceCodeNewRelease() {
         // arrange
-        Movie movie = new Movie(TITLE, Movie.NEW_RELEASE);
-        int daysRented = 1;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(1, Movie.NEW_RELEASE));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t3.0(TITLE_NOT_IMPORTANT)\nAmount owed is 3.0\nYou earned 1 frequent renter pointers", customer.statement());
@@ -65,10 +56,7 @@ public class CustomerTest {
     @Test
     public void testMoviePriceCodeChildrensRentalDaysRented4() {
         // arrange
-        Movie movie = new Movie(TITLE, Movie.CHILDRENS);
-        int daysRented = 4;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(4, Movie.CHILDRENS));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t3.0(TITLE_NOT_IMPORTANT)\nAmount owed is 3.0\nYou earned 1 frequent renter pointers", customer.statement());
@@ -77,10 +65,7 @@ public class CustomerTest {
     @Test
     public void testMoviePriceCodeChildrensRentalDaysRented3() {
         // arrange
-        Movie movie = new Movie(TITLE, Movie.CHILDRENS);
-        int daysRented = 3;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(3, Movie.CHILDRENS));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t1.5(TITLE_NOT_IMPORTANT)\nAmount owed is 1.5\nYou earned 1 frequent renter pointers", customer.statement());
@@ -89,10 +74,7 @@ public class CustomerTest {
     @Test
     public void testMoviePriceCodeNewReleaseRentalDaysRented2() {
         // arrange
-        Movie movie = new Movie(TITLE, Movie.NEW_RELEASE);
-        int daysRented = 2;
-        Rental rental = new Rental(movie, daysRented);
-        customer.addRental(rental);
+        customer.addRental(createRentalFor(2, Movie.NEW_RELEASE));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t6.0(TITLE_NOT_IMPORTANT)\nAmount owed is 6.0\nYou earned 2 frequent renter pointers", customer.statement());
@@ -101,14 +83,15 @@ public class CustomerTest {
     @Test
     public void testMovie3() {
         // arrange
-        Movie regularMovie = new Movie(TITLE, Movie.REGULAR);
-        Movie newReleaseMovie = new Movie(TITLE, Movie.NEW_RELEASE);
-        Movie childrensMovie = new Movie(TITLE, Movie.CHILDRENS);
-        customer.addRental(new Rental(regularMovie, 1));
-        customer.addRental(new Rental(newReleaseMovie, 4));
-        customer.addRental(new Rental(childrensMovie, 4));
+        customer.addRental(createRentalFor(1, Movie.REGULAR));
+        customer.addRental(createRentalFor(4, Movie.NEW_RELEASE));
+        customer.addRental(createRentalFor(4, Movie.CHILDRENS));
 
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t2.0(TITLE_NOT_IMPORTANT)\n\t12.0(TITLE_NOT_IMPORTANT)\n\t3.0(TITLE_NOT_IMPORTANT)\nAmount owed is 17.0\nYou earned 4 frequent renter pointers", customer.statement());
+    }
+
+    private Rental createRentalFor(int daysRented, int priceCode) {
+        return new Rental(new Movie(TITLE, priceCode), daysRented);
     }
 }
