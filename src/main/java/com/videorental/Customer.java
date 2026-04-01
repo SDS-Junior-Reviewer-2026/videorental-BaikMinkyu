30d1f966f69a13dcebc00b59a2b9a97b965565a8
0713jake <0713jake@naver.com>
2026-04-01 16:56:52 +0900

    Extracted getStatementHeader, getRentalLineReport, and getStatementFooter methods, changed iterator reference to enhanced for loop, changed string concatenation in for loop to use StringBuilder, and changed result to be inline

--- src/main/java/com/videorental/Customer.java
+++ src/main/java/com/videorental/Customer.java
@@ -1,7 +1,6 @@
 package com.videorental;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 class Customer {
@@ -21,20 +20,28 @@ class Customer {
 	}
 
 	public String statement() {
-		Iterator<Rental> iterator = rentals.iterator();
-		StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+        return getStatementHeader()
+				+ getRentalLineReport()
+				+ getStatementFooter();
+	}
 
-		while ( iterator.hasNext() ) {
-			Rental each = iterator.next();
-			// determine amounts for each line
-			// show figures
-			result.append("\t").append(each.getCharge()).append("(").append(each.getMovie().getTitle()).append(")").append("\n");
-		}
+	private String getStatementHeader() {
+		return "Rental Record for " + getName() + "\n";
+	}
 
-        result.append("Amount owed is ").append(getTotalAmount()).append("\n");
-		result.append("You earned ").append(getFrequentRenterPoints()).append(" frequent renter pointers");
-
+	private String getRentalLineReport() {
+		StringBuilder result = new StringBuilder();
+        for (Rental rental : rentals) {
+            // determine amounts for each line
+            // show figures
+            result.append("\t").append(rental.getCharge()).append("(").append(rental.getMovie().getTitle()).append(")").append("\n");
+        }
 		return result.toString();
+	}
+
+	private String getStatementFooter() {
+		return "Amount owed is " + getTotalAmount() + "\n" +
+				"You earned " + getFrequentRenterPoints() + " frequent renter pointers";
 	}
 
 	private double getTotalAmount() {
