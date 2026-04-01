89a01a00512340005fdcf10f27148212c3abda90
0713jake <0713jake@naver.com>
2026-04-01 16:45:23 +0900

    Extracted getFrequentRenterPoints and getFrequentRenterPointsFor methods and changed frequentRenderPoints to be inline

--- src/main/java/com/videorental/Customer.java
+++ src/main/java/com/videorental/Customer.java
@@ -21,26 +21,18 @@ class Customer {
 	}
 
 	public String statement() {
-		int frequentRenterPoints = 0;
 		Iterator<Rental> iterator = rentals.iterator();
 		StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
 		while ( iterator.hasNext() ) {
 			Rental each = iterator.next();
 			// determine amounts for each line
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-				frequentRenterPoints++;
-			}
 			// show figures
 			result.append("\t").append(each.getCharge()).append("(").append(each.getMovie().getTitle()).append(")").append("\n");
 		}
 
         result.append("Amount owed is ").append(getTotalAmount()).append("\n");
-		result.append("You earned ").append(frequentRenterPoints).append(" frequent renter pointers");
+		result.append("You earned ").append(getFrequentRenterPoints()).append(" frequent renter pointers");
 
 		return result.toString();
 	}
@@ -51,5 +43,23 @@ class Customer {
 			totalAmount += rental.getCharge();
 		}
 		return totalAmount;
+	}
+
+	private int getFrequentRenterPoints() {
+		int frequentRenterPoints = 0;
+		for (Rental rental : rentals) {
+			frequentRenterPoints += getFrequentRenterPointsFor(rental);
+		}
+		return frequentRenterPoints;
+	}
+
+	private int getFrequentRenterPointsFor(Rental rental) {
+		// add frequent renter points
+		// add bonus for a two days new release rental
+		if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
+			return 2;
+		}
+
+		return 1;
 	}
 }
