415092e6fbae9b315b510d62e9ffe416046e1aae
0713jake <0713jake@naver.com>
2026-04-01 15:46:02 +0900

    Added 'testMoviePriceCodeNewRelease' test

--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -54,4 +54,20 @@ public class CustomerTest {
         // assert
         Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t3.5(TITLE_NOT_IMPORTANT)\nAmount owed is 3.5\nYou earned 1 frequent renter pointers", statement);
     }
+
+    @Test
+    public void testMoviePriceCodeNewRelease() {
+        // arrange
+        Customer customer = new Customer("NAME_NOT_IMPORTANT");
+        Movie movie = new Movie("TITLE_NOT_IMPORTANT", Movie.NEW_RELEASE);
+        int daysRented = 1;
+        Rental rental = new Rental(movie, daysRented);
+        customer.addRental(rental);
+
+        // act
+        String statement = customer.statement();
+
+        // assert
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n\t3.0(TITLE_NOT_IMPORTANT)\nAmount owed is 3.0\nYou earned 1 frequent renter pointers", statement);
+    }
 }
